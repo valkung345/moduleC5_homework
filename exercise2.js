a10b2bf926d8d8d757166528fc0c75ca090e58d8
valkung345 <121906475+valkung345@users.noreply.github.com>
2023-08-10 03:40:40 +0500

    Create exercise2.js

---
+++ exercise2.js
@@ -0,0 +1,18 @@
+const json = `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`;
+   
+   const jsObject = JSON.parse(json);
+   console.log(jsObject);
+   
